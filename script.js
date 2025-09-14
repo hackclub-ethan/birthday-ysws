2c5fb6a3d6a410ca0e8288a3c7aca770870cace3
hackclub-ethan <195668068+hackclub-ethan@users.noreply.github.com>
2025-09-13 23:02:17 -0400

    Basic countdown function

---
+++ script.js
@@ -0,0 +1,60 @@
+const birthday = document.getElementById("birthday");
+const countdown = document.getElementById("countdown");
+
+let currentTime = 0;
+
+function updateTime() {
+    if (currentTime == 0) {
+        return;
+    }
+
+    currentTime -= 1;
+
+    let ownTime = currentTime;
+
+    const days = Math.floor(ownTime / 86400)
+    ownTime = ownTime % 86400;
+    let hours = Math.floor(ownTime / 3600);
+    ownTime = ownTime % 3600;
+    let minutes = Math.floor(ownTime / 60);
+    ownTime = ownTime % 60;
+    let seconds = Math.floor(ownTime);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    };
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    };
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    };
+
+    const formattedTime = `${days}:${hours}:${minutes}:${seconds}`;
+
+    countdown.innerText = formattedTime;
+};
+
+setInterval(updateTime, 1000);
+
+function setBirthday() {
+    const newBirthday = birthday.value;
+    const formattedBirthday = new Date(newBirthday);
+    const rightNow = Date.now();
+
+    const diff = Math.floor((formattedBirthday - rightNow) / 1000);
+
+    console.log(diff)
+
+    if (diff <= 0) {
+        currentTime = 0;
+        alert("Make sure you selected the correct year");
+        return;
+    }
+
+    currentTime = diff;
+}
+
+birthday.addEventListener("change", setBirthday)
